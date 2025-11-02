1d3a0c1f59871f70bdd2f554e897bc469c3d528e
QuantamPX <90332698+QuantamPX@users.noreply.github.com>
2025-11-02 05:43:42 +0000

    added essential plugins

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,27 @@
+return {
+    'nvim-treesitter/nvim-treesitter',
+    build = ":TSUpdate",
+    config = function()
+	local configs = require("nvim-treesitter.configs")
+	configs.setup({
+	    highlight = {
+		enable = true,
+	    },
+	    indent = { enable = true },
+	    autotage = { enable = true },
+	    ensure_installed = {
+		"lua",
+		"c",
+		"cpp",
+		"java",
+		"javascript",
+		"html",
+		"css",
+		"go",
+		"python",
+		"bash",
+	    },
+	    auto_install = false,
+	})
+    end
+}
